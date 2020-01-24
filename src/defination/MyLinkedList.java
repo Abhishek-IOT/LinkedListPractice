7b76ccaec360e43581f7f0adffe99c31c037c873
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-24 15:56:06 +0530

    implement method of LinkedListADT

--- src/defination/MyLinkedList.java
+++ src/defination/MyLinkedList.java
@@ -1,5 +1,27 @@
 package defination;
 
-public class MyLinkedList<E> {
+import adt.LinkedListADT;
 
+public class MyLinkedList<E> implements LinkedListADT<E> {
+
+
+    public void add(E item) {
+
+    }
+
+    public E remove(E item) {
+        return null;
+    }
+
+    public int search(E item) {
+        return 0;
+    }
+
+    public void sort() {
+
+    }
+
+    public void print() {
+
+    }
 }
